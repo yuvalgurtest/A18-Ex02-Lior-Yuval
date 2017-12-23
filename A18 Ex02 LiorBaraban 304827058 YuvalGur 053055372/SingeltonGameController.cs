51a7bf0f39ef6d1848733b75655a16e364adb81b
Yuval Gur <yuval.gur@gmail.com>
2017-12-23 14:17:13 +0200

    first time Singelton implementation

---
+++ A18 Ex02 LiorBaraban 304827058 YuvalGur 053055372/SingeltonGameController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace A18_Ex01_Lior_Yuval
+{
+    public sealed class SingeltonGameController
+    {
+        private static SomeSingleton5 s_Instance = null;
+        private static object s_LockObj = new Object();
+
+        private SomeSingleton5() { }
+
+        public static SomeSingleton5 Instance
+        {
+            get
+            {
+                if (s_Instance == null)
+                {
+                    lock (s_LockObj)
+                    {
+                        if (s_Instance == null)
+                        {
+                            s_Instance = new SomeSingleton5();
+                        }
+                    }
+                }
+
+                return s_Instance;
+            }
+        }
+    }
+}
